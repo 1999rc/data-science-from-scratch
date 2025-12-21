02ddee6151d43d396a2dae0e1dc770dafd6a52c2
Raees chishty <mohommedraees@gmail.com>
2025-12-21 16:53:04 +0530

    Add experimental min-max normalization utility

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,13 @@
 def greet(name):
     
     return f'Hello,{name}!Welcome to Data Science From Scratch'
+def normalize_data(values):
+    '''
+    Experimental normalizatio(min-max scaling)
+    Warning:Expereimental logic --- not 
+    production-ready
+    '''
+    min_val=min(values)
+    max_val=max(values)
+    return [(v-min_val)/(max_val-min_val)for v in values]
+    
