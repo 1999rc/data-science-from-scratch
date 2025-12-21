c0bac0c97ae4a47c7e83832e15636d443b086278
Raees chishty <mohommedraees@gmail.com>
2025-12-21 14:23:41 +0530

    Add docstring to welcome function for clarity

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,12 @@
 def welcome():
+    """
+    Prints a welcome message for the Data Science
+    From Scratch project
+
+    This function is the first example of
+    - Writing clean Python functions 
+    -Keeping logic inside src/
+    -Using main.py as an entry point
+    """
     print('welcome to Data Science From Scratch')
     print('Learning Python:Linux,Git,Linux the right way')
