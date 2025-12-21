e1dadb2305faef73829c2a24ecfc4e9fb5686863
Raees chishty <mohommedraees@gmail.com>
2025-12-21 15:25:45 +0530

    Use src package and fix imports correctly

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-"""
-Main entry point for the Data Science From
-Scratch project 
-THis file responsible for:
--Starting the program 
--Calling functions from the src/directory 
--Keeping execution logic seperate from 
-implementation"""
-from src.utils import welcome
+from src.utils import greet 
 def main():
-    """
-    Main function that runs the application
-    """
-    welcome()
+    message=greet('Learner')
+    print(message)
+    
 if __name__=='__main__':
     main()
-    
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,6 @@
-def welcome():
+def greet(name):
     """
-    Prints a welcome message for the Data Science
-    From Scratch project
-
-    This function is the first example of
-    - Writing clean Python functions 
-    -Keeping logic inside src/
-    -Using main.py as an entry point
+    This function takes a name and returns a
+    greeting message
     """
-    print('welcome to Data Science From Scratch')
-    print('Learning Python:Linux,Git,Linux the right way')
+    return f'Hello,{name}!Welcome to Data Science From Scratch'
