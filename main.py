6d7e7b19487142a4c951ce2e591b6a10f54a3092
Raees chishty <mohommedraees@gmail.com>
2025-12-21 14:31:43 +0530

    Refactor main.py to use entry point pattern

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+"""
+Main entry point for the Data Science From
+Scratch project 
+THis file responsible for:
+-Starting the program 
+-Calling functions from the src/directory 
+-Keeping execution logic seperate from 
+implementation"""
 from src.utils import welcome
-welcome()
+def main():
+    """
+    Main function that runs the application
+    """
+    welcome()
+if __name__=='__main__':
+    main()
+    
