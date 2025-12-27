725ef22be2ef56ad35c7f936dc6e9f630d224629
Raees chishty <mohommedraees@gmail.com>
2025-12-27 09:16:28 +0530

    Connect SimpleNN to Bee for decision-based movement

--- src/models/bee.py
+++ src/models/bee.py
@@ -1,9 +1,11 @@
+from .nn import SimpleNN
 class Bee:
     def __init__(self,start_x=0,start_y=0):
         #current position 
         self.start_x=start_x
         self.start_y=start_y
         self.memory=Memory()
+        self.brain=SimpleNN()
         #self.path_memory=[(self.x,self.y)]
         #journey state
          #+1 -> going out for look honey(searching)
@@ -35,4 +37,13 @@ class Bee:
             self.x,self.y=self.path_memory[-1]'''
     def position(self):
         return self.x,self.y 
-        
+    def move_using_nn(self,target_x,target_y):
+        """
+        Bee decides movement using neural network
+        """
+        dx=target_x-self.x 
+        dy=target_y-self.y 
+        actionself.brain.forward(dx,dy)
+        self.x+=action 
+        self.memory.remember(self.x,self.y)
+        return action
